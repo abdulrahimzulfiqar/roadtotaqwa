061019b18f8257c6ed39050d2bdccd95141d0fc4
Abdulraheem <abdulraheemzulfiqar2232@gmail.com>
2024-05-08 16:43:24 +0500

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<section>
-  <div className="container mx-auto flex p-4 md:p-12 items-center justify-center">Hello</div>
-</section>
+<>
+
+</>
   )
 }
